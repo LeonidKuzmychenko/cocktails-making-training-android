fa7bf147d2fd05f52d8408a042b11bee29392697
leonid.kuzmychenko <leonid.kuzmychenko@ab-soft.net>
2021-04-08 14:49:20 +0300

    test1

--- app/src/main/java/lk/game/cocktails/MainActivity.kt
+++ app/src/main/java/lk/game/cocktails/MainActivity.kt
@@ -1,14 +1,16 @@
 package lk.game.cocktails
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d(TAG, "test")
     }
 
 }
