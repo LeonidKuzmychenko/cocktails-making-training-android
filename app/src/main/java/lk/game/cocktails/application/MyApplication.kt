5193f5584875a8fc112659cd7bcf686004e55036
leonid.kuzmychenko <leonid.kuzmychenko@ab-soft.net>
2021-04-16 00:08:57 +0300

    remove protobuf +

--- app/src/main/java/lk/game/cocktails/application/MyApplication.kt
+++ app/src/main/java/lk/game/cocktails/application/MyApplication.kt
@@ -3,7 +3,6 @@ package lk.game.cocktails.application
 import android.app.Application
 import lk.game.cocktails.dagger.DaggerWebComponent
 import lk.game.cocktails.dagger.WebComponent
-import lk.game.cocktails.dagger.modules.AppModule
 
 class MyApplication : Application() {
 
@@ -15,9 +14,7 @@ class MyApplication : Application() {
 //            .webModule(WebModule())
 //            .build()
 
-        webComponent = DaggerWebComponent.builder()
-            .appModule(AppModule(this))
-            .build()
+        webComponent = DaggerWebComponent.create()
     }
 
     fun getWebComponent(): WebComponent {
