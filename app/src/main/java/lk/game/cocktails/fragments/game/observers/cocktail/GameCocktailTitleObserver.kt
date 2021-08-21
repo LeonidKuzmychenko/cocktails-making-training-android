910e4f2b1fa9160fa9bc15f7b04bdce9b5b6e2ee
Leonid Kuzmychenko <lleonidk@ukr.net>
2021-08-21 19:37:36 +0300

    fix title size

--- app/src/main/java/lk/game/cocktails/fragments/game/observers/cocktail/GameCocktailTitleObserver.kt
+++ app/src/main/java/lk/game/cocktails/fragments/game/observers/cocktail/GameCocktailTitleObserver.kt
@@ -12,11 +12,12 @@ class GameCocktailTitleObserver(
 ) : Observer<Cocktail> {
 
     override fun onChanged(cocktail: Cocktail) {
-        activity.supportActionBar!!.title = cocktail.name
+        val spannableName = SpannableString(cocktail.name)
+        spannableName.setSpan(RelativeSizeSpan(0.8f), 0, spannableName.length, 0)
+        activity.supportActionBar!!.title = spannableName
 
         val spannableAssociation = SpannableString(cocktail.association)
         spannableAssociation.setSpan(RelativeSizeSpan(0.7f), 0, spannableAssociation.length, 0)
-
         activity.supportActionBar!!.subtitle = spannableAssociation
     }
 
